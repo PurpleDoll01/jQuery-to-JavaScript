cc3583e249aadd2fd2c60803d7de23dd3cd996db
Mariana Valencia <purple.mv@gmail.com>
2020-01-14 17:40:50 -0500

    Fetching data using the form

--- src/js/home.js
+++ src/js/home.js
@@ -68,7 +68,25 @@ fetch('https://randomuser.me/api/')
       }
     }
 
-    $form.addEventListener('submit', (event) => {
+    const BASE_API = 'https://yts.lt/api/v2/';
+
+    function featuringTemplate(peli) {
+      return (
+        `
+        <div class="featuring">
+         <div class="featuring-image">
+           <img src="${peli.medium_cover_image}" width="70" height="100" alt="">
+         </div>
+         <div class="featuring-content">
+           <p class="featuring-title">Pelicula encontrada</p>
+           <p class="featuring-album">${peli.title}</p>
+         </div>
+        </div>
+        `        
+      )
+    }
+
+    $form.addEventListener('submit', async (event) => {
       event.preventDefault();  
       $home.classList.add('search-active');
       const $loader = document.createElement('img');
@@ -78,6 +96,11 @@ fetch('https://randomuser.me/api/')
         width: 50,
       })
       $featuringContainer.append($loader);
+
+      const data = new FormData($form);
+      const peli = await getData(`${BASE_API}list_movies.json?limit=1&query_term=${data.get('name')}`)
+      const HTMLString = featuringTemplate(peli.data.movies[0]);
+      $featuringContainer.innerHTML = HTMLString;
     })
 
     const actionList = await getData('https://yts.lt/api/v2/list_movies.json?genre=action');  
